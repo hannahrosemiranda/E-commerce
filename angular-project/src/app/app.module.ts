d5d93e2d281f8acb49ba8f41d91a20417004ea5c
Miranda, Hannah <hannah.rose.miranda@accenture.com>
2023-06-19 15:50:20 +0800

    products

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
