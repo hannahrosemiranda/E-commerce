7d2b65e4ba55a5d46125f0b250b20c67efcd1b5a
Miranda, Hannah <hannah.rose.miranda@accenture.com>
2023-06-21 15:04:00 +0800

    products added

---
+++ angular-project/src/data.ts
@@ -0,0 +1,164 @@
+import { Product } from './app/shared/models/product';
+
+export const sample_products: Product[] = [
+{
+  id: '1',
+  name: 'Mens Black Shoes',
+  price: 2000,
+  description: 'Black shoes',
+  imageUrl: 'assets/images/photo1.png',
+},
+
+{
+  id: '2',
+  name: 'Mens sneakers',
+  price: 1500,
+  description: 'sneakers',
+  imageUrl: 'assets/images/photo2.png',
+},
+
+{
+  id: '3',
+  name: 'Mens Black Sneakers',
+  price: 2000,
+  description: 'Black shoes',
+  imageUrl: 'assets/images/photo3.png',
+},
+
+{
+  id: '4',
+  name: 'Mens Black Rubber Shoes',
+  price: 1000,
+  description: 'Rubber Shoes',
+  imageUrl: 'assets/images/photo4.png',
+},
+
+{
+  id: '5',
+  name: 'Basketball Shoes',
+  price: 3000,
+  description: 'Basketball shoes',
+  imageUrl: 'assets/images/photo5.png',
+},
+
+{
+  id: '6',
+  name: 'Mens Denim Polo',
+  price: 1500,
+  description: 'Polo Shirt',
+  imageUrl: 'assets/images/photo6.png',
+},
+
+{
+  id: '7',
+  name: 'Mens Checkered Polo',
+  price: 900,
+  description: 'Chekered Polo',
+  imageUrl: 'assets/images/photo7.png',
+},
+
+{
+  id: '8',
+  name: 'Mens Striped Polo',
+  price: 900,
+  description: 'Striped Polo',
+  imageUrl: 'assets/images/photo8.png',
+},
+
+{
+  id: '9',
+  name: 'Mens Long Sleeves Polo',
+  price: 1500,
+  description: 'Long Sleeves Polo',
+  imageUrl: 'assets/images/photo9.png',
+},
+
+{
+  id: '10',
+  name: 'Mens Summer Polo',
+  price: 900,
+  description: 'Summer Polo',
+  imageUrl: 'assets/images/photo10.png',
+},
+
+{
+  id: '11',
+  name: 'Womens Black Dress',
+  price: 2000,
+  description: 'Black Dress',
+  imageUrl: 'assets/images/photo11.png',
+},
+
+{
+  id: '12',
+  name: 'Womens Black Shirt',
+  price: 900,
+  description: 'Black Shirt',
+  imageUrl: 'assets/images/photo12.png',
+},
+
+{
+  id: '13',
+  name: 'Womens Summer Dress',
+  price: 900,
+  description: 'Summer Dress',
+  imageUrl: 'assets/images/photo13.png',
+},
+
+{
+  id: '14',
+  name: 'Womens White Polo',
+  price: 950,
+  description: 'White Polo',
+  imageUrl: 'assets/images/photo14.png',
+},
+
+{
+  id: '15',
+  name: 'Khaki Dress',
+  price: 850,
+  description: 'Dress',
+  imageUrl: 'assets/images/photo15.png',
+},
+
+{
+  id: '16',
+  name: 'Womens sneakers',
+  price: 2000,
+  description: 'Sneakers',
+  imageUrl: 'assets/images/photo16.png',
+},
+
+{
+  id: '17',
+  name: 'Womens sneakers',
+  price: 3000,
+  description: 'Sneakers',
+  imageUrl: 'assets/images/photo17.png',
+},
+
+{
+  id: '18',
+  name: 'Womens Flipflops',
+  price: 2000,
+  description: 'Flipflops',
+  imageUrl: 'assets/images/photo18.png',
+},
+
+{
+  id: '19',
+  name: 'Womens White Shoes',
+  price: 2000,
+  description: 'White Shoes',
+  imageUrl: 'assets/images/photo19.png',
+},
+
+{
+  id: '20',
+  name: 'Womens Sandals',
+  price: 2000,
+  description: 'Sandals',
+  imageUrl: 'assets/images/photo20.png',
+},
+
+]
